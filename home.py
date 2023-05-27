c7661a9ec96cd6c3df25087a9e9316b4348fe377
edwinjuan <edwinjuan25@gmail.com>
2023-05-28 01:43:46 +0700

    new update

--- home.py
+++ home.py
@@ -7,6 +7,7 @@ import altair as alt
 from sklearn.metrics import silhouette_score
 from sklearn.cluster import KMeans
 import plotly.graph_objects as go
+import time
 
 st.set_page_config(layout="wide")
 # Hide setting menu
@@ -26,11 +27,33 @@ if 'token' not in st.session_state:
 
 def main():
      # ---- Preparing data Streamlit Login ---- 
-    baseURL = 'http://127.0.0.1:8000/api'
+    baseURL = 'http://api.dashuajy-admin.xyz/api'
     #st.markdown(hide_menu, unsafe_allow_html=True)
-    # users = database_func.get_all_user()
-    response = requests.get(baseURL + '/users')
+    cookies = {
+        'XSRF-TOKEN': 'eyJpdiI6Ik5zeFlvV3RXNUhsNEFlWGNHVjlzMlE9PSIsInZhbHVlIjoiU2tWOEwrOERzd29DR09LZ1JpUmtSMlhDOHVya3RiTCs0Y3J0a2pDekxmbDYyeFpqZ0tjcHJNL0JBczJpbEtCOVBGcUlJbzdpR3dzM2xKL2dCZTlmaVYyYUlwOGJIb0s5a0pjQ0dnam1sTXJMeFVTVmZiT1l2V2RBUERURG9ZU08iLCJtYWMiOiJmMzU1MzRkNTQ1MGU0OTJkMDZiYmRlODA5OWM3MThmMDViMjliMTFhZTA5MmNjZmU1ZTU0YmMyYmMxYjIyZTcwIiwidGFnIjoiIn0%3D',
+        'laravel_session': 'eyJpdiI6InVXdTFNdWpEYkNzMjArYzMrbDQzblE9PSIsInZhbHVlIjoiTnZNc3VyK3RXRFY5ajlLWkNEazR0aVhWR0VaRitMNE14MnVkLy9nNGZMTHN6UXpTQTlhcDBMMExUdWloSEdZZ3h1NGg5VjlMZUlJdEJGMjlGKzNpWnZSSTE4ajJmTlYvTTNaTDFPTFJzTFVFbVFRVTdqblZFb2tXaENMU1RVZnciLCJtYWMiOiI4YjU5NjFhOTk0M2UzMmJlYjYyODZiZTAzMTViNzllMDk2NmI3ODk1YjNiZmRiMTRjNDhmYTY5ZjUxMjk1YjVhIiwidGFnIjoiIn0%3D',
+    }
+    headers = {
+        'authority': 'api.dashuajy-admin.xyz',
+        'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,/;q=0.8,application/signed-exchange;v=b3;q=0.7',
+        'accept-language': 'en-US,en-GB;q=0.9,en;q=0.8',
+        'cache-control': 'max-age=0',
+        # 'cookie': 'XSRF-TOKEN=eyJpdiI6Ik5zeFlvV3RXNUhsNEFlWGNHVjlzMlE9PSIsInZhbHVlIjoiU2tWOEwrOERzd29DR09LZ1JpUmtSMlhDOHVya3RiTCs0Y3J0a2pDekxmbDYyeFpqZ0tjcHJNL0JBczJpbEtCOVBGcUlJbzdpR3dzM2xKL2dCZTlmaVYyYUlwOGJIb0s5a0pjQ0dnam1sTXJMeFVTVmZiT1l2V2RBUERURG9ZU08iLCJtYWMiOiJmMzU1MzRkNTQ1MGU0OTJkMDZiYmRlODA5OWM3MThmMDViMjliMTFhZTA5MmNjZmU1ZTU0YmMyYmMxYjIyZTcwIiwidGFnIjoiIn0%3D; laravel_session=eyJpdiI6InVXdTFNdWpEYkNzMjArYzMrbDQzblE9PSIsInZhbHVlIjoiTnZNc3VyK3RXRFY5ajlLWkNEazR0aVhWR0VaRitMNE14MnVkLy9nNGZMTHN6UXpTQTlhcDBMMExUdWloSEdZZ3h1NGg5VjlMZUlJdEJGMjlGKzNpWnZSSTE4ajJmTlYvTTNaTDFPTFJzTFVFbVFRVTdqblZFb2tXaENMU1RVZnciLCJtYWMiOiI4YjU5NjFhOTk0M2UzMmJlYjYyODZiZTAzMTViNzllMDk2NmI3ODk1YjNiZmRiMTRjNDhmYTY5ZjUxMjk1YjVhIiwidGFnIjoiIn0%3D',
+        'dnt': '1',
+        'sec-ch-ua': '"Google Chrome";v="113", "Chromium";v="113", "Not-A.Brand";v="24"',
+        'sec-ch-ua-mobile': '?1',
+        'sec-ch-ua-platform': '"Android"',
+        'sec-fetch-dest': 'document',
+        'sec-fetch-mode': 'navigate',
+        'sec-fetch-site': 'none',
+        'sec-fetch-user': '?1',
+        'sec-gpc': '1',
+        'upgrade-insecure-requests': '1',
+        'user-agent': 'Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/113.0.0.0 Mobile Safari/537.36',
+    }
 
+    response = requests.get(baseURL + '/users', cookies=cookies, headers=headers)
+    
     credentials = {"usernames": {}}
     for data in response.json()['data']:
         user_dict = {"name": data['users_id'], "password": data['password']}
@@ -53,7 +76,7 @@ def main():
             st.markdown('###')
             authenticator.logout('Logout', 'main') 
         # Filter kelas
-        response = requests.get(baseURL + '/class/st/show/' + str(st.session_state['name']))
+        response = requests.get(baseURL + '/class/st/show/' + str(st.session_state['name']), cookies=cookies, headers=headers)
         optionsClass = {}
         for data in response.json()['data']:
             key = data['code'] + ' - ' + data['matkul_name'] + ' - ' + data['class_order']
@@ -64,9 +87,9 @@ def main():
         selected_class_id = optionsClass[selected_class]
 
         # Get Data Dashboard Class
-        response = requests.get(baseURL + '/class/dashboard/' + str(selected_class_id))
-        responseAsgn = requests.get(baseURL + '/class/dashboard/asgn/' + str(selected_class_id))
-        responseCpmk = requests.get(baseURL + '/class/dashboard/matkul/' + str(selected_class_id))
+        response = requests.get(baseURL + '/class/dashboard/' + str(selected_class_id), cookies=cookies, headers=headers)
+        responseAsgn = requests.get(baseURL + '/class/dashboard/asgn/' + str(selected_class_id), cookies=cookies, headers=headers)
+        responseCpmk = requests.get(baseURL + '/class/dashboard/matkul/' + str(selected_class_id), cookies=cookies, headers=headers)
 
         if (response.status_code == 200):
             # --- Get and preparing All Data ---
@@ -118,7 +141,9 @@ def main():
                 # calculate final weighted grade
                 colWeightName = "weighted {}".format(col)
                 percent = df_cpmk[df_cpmk['cpmk_id'] == int(col)]['percentage'].values[0]
-                df_total_grouped_sum[colWeightName] = df_total_grouped_sum[colTotName] * (percent/100)            
+                df_total_grouped_sum[colWeightName] = df_total_grouped_sum[colTotName] * (percent/100)
+            #fill NaN with 0
+            df_total_grouped_sum.fillna(0, inplace=True)       
             # calculate final grade
             for i, row in df_total_grouped_sum.iterrows():
                 total = 0
@@ -259,7 +284,10 @@ def main():
             st.markdown('###') # Separator
             
             # Set Column
-            st.subheader('Average CPMK Grade')
+
+            colHeadCpmk, colFilterCpmk = st.columns([3,2])
+            colHeadCpmk.markdown('###')
+            colHeadCpmk.subheader('CPMK Grade')
             cols = st.columns(df_cpmk.shape[0])
             col1, col2 = st.columns([3,2])
             col3, col4 = st.columns([3,2])
@@ -272,12 +300,14 @@ def main():
             data = df[df['assignment_description'] == selected_assignment]
             avg_grade = data["grade"].mean()
             data["Above Average"] = data["grade"].apply(lambda x: 'Yes' if x > avg_grade else 'No')
+            # make column have proper name
+            data.rename(columns = {'name':'Name', 'grade':'Grade'}, inplace = True)
             #charting
             chart2 = alt.Chart(data, height=500).mark_bar().encode(
-                x=alt.X("npm", sort=None),
-                y=alt.Y('grade', axis=alt.Axis(title='Grade')),
+                x=alt.X("npm", axis=alt.Axis(title='NPM')),
+                y=alt.Y('Grade', axis=alt.Axis(title='Grade')),
                 color=alt.Color("Above Average:N", scale=alt.Scale(domain=['Yes', 'No'], range=["green", "red"])),
-                tooltip=["name", "grade", "Above Average"]
+                tooltip=["Name", "Grade", "Above Average"]
             ).properties(
                 title=f"(Average Grade = {avg_grade:.2f})"
             )
@@ -317,16 +347,18 @@ def main():
                 finalGrade = df_grade_total[df_grade_total['npm'] == data['npm']]['final'].values[0]
                 return finalGrade
             chart_data['Final Grade'] = chart_data.apply(putFinalGrade, axis=1)
+            # Make column name more proper
+            chart_data.rename(columns = {'name':'Name', 'npm':'NPM', 'cpmk':'CPMK'}, inplace = True)
             # Charting
             chart = alt.Chart(chart_data).mark_bar().encode(
                 x=alt.X('Weighted Grade:Q'),
-                y=alt.Y('npm:N', sort='-x'),
-                color=alt.Color('cpmk:N', legend=alt.Legend(
+                y=alt.Y('NPM:N', sort='-x', axis=alt.Axis(title='NPM')),
+                color=alt.Color('CPMK:N', legend=alt.Legend(
                     orient='top',
                     legendX=130, legendY=-40,
                     direction='horizontal',
                     title=None)),
-                tooltip=["name", "npm", 'cpmk', 'Weighted Grade', 'Grade', 'Final Grade']
+                tooltip=["Name", "NPM", 'CPMK', 'Weighted Grade', 'Grade', 'Final Grade']
             ).properties(
                 height=600,
                 title='Top 10 Students Performance'
@@ -338,7 +370,8 @@ def main():
 
             # --- Performance Ratio ---
             # Clustering
-            features = df_grade_total[['final']].values
+            df_grade_total2 = df_grade_total.dropna(subset=['final'])
+            features = df_grade_total2[['final']].values
             max_clusters = 8  # Maximum number of clusters to consider
             best_score = -1
             optimal_clusters = None
@@ -354,9 +387,9 @@ def main():
             kmeans = KMeans(n_clusters=optimal_clusters, random_state=0)
             labels = kmeans.fit_predict(features)
             # Add the cluster labels to the original DataFrame
-            df_grade_total['Cluster'] = labels
+            df_grade_total2['Cluster'] = labels
             # Calculate the percentage of students in each cluster
-            cluster_percentage = df_grade_total['Cluster'].value_counts(normalize=True).reset_index()
+            cluster_percentage = df_grade_total2['Cluster'].value_counts(normalize=True).reset_index()
             cluster_percentage.columns = ['Cluster', 'Percentage']
             cluster_percentage['data'] = 'data'
             cluster_percentage['Percentage'] = cluster_percentage['Percentage'] * 100
@@ -384,7 +417,7 @@ def main():
 
             # --- scatter plot for cluster distribution ---
             # Change column name from final to grade
-            df_grade_cluster = df_grade_total.rename(columns={'final': 'Grade'})
+            df_grade_cluster = df_grade_total2.rename(columns={'final': 'Grade'})
             # Define the color scale for the scatter plot
             # Get unique cluster values
             clusters = df_grade_cluster['Cluster'].unique().tolist()
@@ -392,9 +425,11 @@ def main():
                 domain=clusters,
                 range=['#ff0000', '#00ff00', '#0000ff', '#ffff00', '#00fff7', '#ff00d0', '#ff9900', '#ff0077']  # Add more colors as needed
             )
+            # Make column name more proper
+            df_grade_cluster.rename(columns = {'npm':'NPM'}, inplace = True)
             # Plotting Scatter plot
             parallel_plot = alt.Chart(df_grade_cluster).mark_circle(size=50).encode(
-                x=alt.X('npm:O', axis=alt.Axis(labels=True)),
+                x=alt.X('NPM:O', axis=alt.Axis(labels=True)),
                 color=alt.Color('Cluster:N', scale=color_scale),
                 detail='Cluster:N',
                 y=alt.Y(alt.repeat("column"), type='quantitative')
@@ -411,6 +446,9 @@ def main():
             col4.altair_chart(parallel_plot, use_container_width=True)
 
             # --- Show remaining grade needed ---
+            #df_remain = df_grade_total.copy()
+            #st.dataframe(df_remain)
+            #st.dataframe(df_cpmk)
             # if (int(df['percentage'].unique().sum()) < 100):
             #     col3.subheader('__Nilai minimum di sisah assignment untuk lulus__')
             #     data = df.copy()
@@ -454,6 +492,13 @@ def main():
             df_melted = df_grouped_assign.melt(id_vars=['assignment_id', 'assignment_description'], var_name='CPMK', value_name='Grade')
             df_melted.fillna(0, inplace=True)
             df_filtered = df_melted[df_melted['Grade'] != 0]
+            # Make column name more proper
+            df_filtered.rename(columns = {'assignment_description':'Assignment Description'}, inplace = True)
+            # calculate percent
+            percent_cpmk = df_filtered.groupby(['CPMK'])['Grade'].sum().reset_index()
+            for i, row in df_filtered.iterrows():
+                tot = percent_cpmk[percent_cpmk['CPMK'] == row['CPMK']]['Grade'].values
+                df_filtered.loc[i, 'Percentage'] = row['Grade'] / tot * 100
             # Calculate the average grade for each CPMK
             df_avg_grade = df_filtered.groupby('CPMK')['Grade'].mean().reset_index()
             df_avg_grade.sort_values(by=['CPMK'], inplace=True)
@@ -474,15 +519,15 @@ def main():
                     CPMKNumber = CPMKNumber + 1
                     # Create the pie chart
                     base = alt.Chart(df_cpmk2).encode(
-                        alt.Color('assignment_description:N', legend=alt.Legend(title='Assignment', orient='top')),
-                        theta=alt.Theta('Grade:Q', stack=True),
-                        tooltip=['assignment_description', 'Grade']
+                        alt.Color('Assignment Description:N', legend=alt.Legend(title='Assignment', orient='top')),
+                        theta=alt.Theta('Percentage:Q', stack=True),
+                        tooltip=['Assignment Description', 'Grade', 'Percentage']
                     ).properties(
                         title=f'CPMK {CPMKNumber}'
                     )
                     # Add text marks to show the numbers on each slice
                     pie = base.mark_arc(outerRadius=120)
-                    text = base.mark_text(radius=145, size=14).encode(text="Grade:Q")
+                    text = base.mark_text(radius=145, size=14).encode(text=alt.Tooltip("Percentage:Q", format=".1f"))
                     # add all chart
                     chart = (pie + text)
                     row_charts.append(chart)
@@ -496,15 +541,26 @@ def main():
 
 
             # --- Show Progress CPMK ---
+            #Select box untuk filter CPMK
+            df_student_name = df_grade_total[['npm', 'name']]
+            df_student_name.drop_duplicates(subset="npm", inplace=True)
+            listCPMKFilter = ["{}-{}".format(str(x['npm']), str(x['name'])) for i,x in df_student_name.iterrows()]
+            listCPMKFilter.insert(0, 'Average Class')
+            selected_cpmk_filter = colFilterCpmk.selectbox('Student Filter', options=listCPMKFilter) 
             # Preparing data
             df_data = df_cpmk.copy()
             df_data['average'] = 0
-            avg_cpmk_list = {}
-            for cpmk in list_cpmk_col:
-                colName = "average {}".format(cpmk)
-                avg = df_grade_total[colName].mean()
-                df_data.loc[df_data['cpmk_id'] == int(cpmk), 'average'] = avg
-            
+            if (selected_cpmk_filter == "Average Class"):
+                for cpmk in list_cpmk_col:
+                    colName = "average {}".format(cpmk)
+                    avg = df_grade_total[colName].mean()
+                    df_data.loc[df_data['cpmk_id'] == int(cpmk), 'average'] = avg
+            else:
+                npm = selected_cpmk_filter.split('-')[0]
+                data = df_grade_total[df_grade_total['npm'] == npm]
+                for cpmk in list_cpmk_col:
+                    colName = "average {}".format(cpmk)
+                    df_data.loc[df_data['cpmk_id'] == int(cpmk), 'average'] = data[colName].values
             # Create a Gauge chart
             for i, column in enumerate(cols):
                 #description = df_cpmk[df_cpmk['cpmk_id'] == data_cpmk.loc[i, 'cpmk_id']]['description'].item()
